296dd7271313d1d2350c57e573cdb9280172d9b0
kadduni <ynnyself19@gmail.com>
2026-06-10 02:59:58 +0800

    Add comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,37 @@
-// todays date ⏔⏔⏔
+// ⏔⏔⏔ todays date ⏔⏔⏔
 const date = new Date();
 const day = date.getDate();
 let month = date.getMonth();
 let year = date.getFullYear();
-let lastDay = new Date(year, month + 1, 0).getDate();
 
 const noteInput = document.getElementById('note');
 let id = '';
 
-// arrays ⏔⏔⏔
+// ⏔⏔⏔ arrays ⏔⏔⏔
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-// display month and year ⏔⏔⏔
+// ⏔⏔⏔ display month and year ⏔⏔⏔
 function displayMonthYear(monthValue, yearValue) {
   const monthLabel = document.getElementById('month');
   const yearLabel = document.getElementById('year');
   
+  // Change the innerHTML
   monthLabel.innerHTML = monthValue;
   yearLabel.innerHTML = year;
 
   // display selected month & year indicator in the panel
-  selectedMonthYear(monthValue, yearValue);
+  selectMonthYear(monthValue, yearValue);
+
   displayDays();
 }
 
-// arrow buttons ⏔⏔⏔
+// ⏔⏔⏔ arrow buttons ⏔⏔⏔
 const prevButton = document.getElementById('previous-button');
 const nextButton = document.getElementById('next-button');
 
 prevButton.addEventListener('click', () => {
+  // If month goes down to Jan, jump to Dec and go a year down
   if (month > 0) {
     month--;
   } else {
@@ -41,6 +43,7 @@ prevButton.addEventListener('click', () => {
 });
 
 nextButton.addEventListener('click', () => {
+  // If month goes up to December, go back to January and go a year up
   if (month < 11) {
     month++;
   } else {
@@ -51,35 +54,39 @@ nextButton.addEventListener('click', () => {
   displayMonthYear(months[month], year);
 });
 
-// month and year panel ⏔⏔⏔
+// ⏔⏔⏔ month and year panel ⏔⏔⏔
 const monthYearLabel = document.getElementById('month-year');
 
 // show month and year panel
 const monthYearPanel = document.getElementById('month-year-panel');
 
+// when the label gets clicked, show the panel
 monthYearLabel.addEventListener('click', () => {
   monthYearPanel.classList.remove('hidden');
 });
 
-
-
-function selectedMonthYear(monthValue, yearValue) {
+// ⏔⏔⏔ jump onto a specific month & year ⏔⏔⏔
+function selectMonthYear(monthValue, yearValue) {
+  // toggle and untoggle buttons feature
   document.querySelectorAll('.month-name')
     .forEach((monthButton) => {
       monthButton.classList.remove('selected');
 
+      // get the dataset stored in the element when clicked
       monthButton.addEventListener('click', () => {
         const monthId = monthButton.dataset.month;
 
+        // for all elements with this class, remove class 'selected'
         document.querySelectorAll('.month-name')
           .forEach((monthButton) => {
             monthButton.classList.remove('selected');
           });
 
+        // add 'selected' class for the element with the class of returned dataset
         document.querySelector(`.month-name-${monthId}`)
           .classList.add('selected');
 
-        console.log(monthId)
+        console.log(monthId);
       });
     });
 
@@ -101,6 +108,8 @@ function selectedMonthYear(monthValue, yearValue) {
       });
     });
 
+  // automatically selects the current month and year
+  // store the month index of the month that matched the monthValue parameter
   let monthIndex = 0;
 
   for (let i = 0; i < months.length; i++) {
@@ -109,6 +118,7 @@ function selectedMonthYear(monthValue, yearValue) {
     }
   }
 
+  // modify the element and add the 'selected' class
   document.querySelector(`.month-name-${monthIndex}`)
     .classList.add('selected');
 
@@ -116,7 +126,8 @@ function selectedMonthYear(monthValue, yearValue) {
     .classList.add('selected');
 }
 
-// months container ⏔⏔⏔
+// ⏔⏔⏔ months container ⏔⏔⏔
+// generates months html
 const monthsContainer = document.getElementById('months-container');
 
 let monthsHTML = '';
@@ -129,10 +140,10 @@ months.forEach((month, index) => {
 
 monthsContainer.innerHTML = monthsHTML;
 
-// years container ⏔⏔⏔
+// ⏔⏔⏔ years container ⏔⏔⏔
+// generate years, 2000 - 2026 + 50
 const yearsContainer = document.getElementById('years-container');
 
-// generate years
 let yearsHTML = '';
 
 for (let i = 2000; i <= year + 50; i++) {
@@ -143,31 +154,36 @@ for (let i = 2000; i <= year + 50; i++) {
 
 yearsContainer.innerHTML = yearsHTML;
 
-// close month year panel ⏔⏔⏔
+// ⏔⏔⏔ close button - hide month year panel ⏔⏔⏔
 const closeMonthYearButton = document.getElementById('close-month-year-panel');
 
 closeMonthYearButton.addEventListener('click', () => {
   monthYearPanel.classList.add('hidden');
 });
 
-// days ⏔⏔⏔
+// ⏔⏔⏔ display days ⏔⏔⏔
 function displayDays() {
-  const selectedDate = new Date(year + '-' + (month + 1) + "-01");
-  let daySelected = '';
-  lastDay = new Date(year, month + 1, 0).getDate();
+  // get the first day to identify the weekday
+  const firstDay = new Date(year + '-' + (month + 1) + "-01");
 
+  // get the last day to identify days count in a month
+  const lastDay = new Date(year, month + 1, 0).getDate();
+
+  // store day selected and weekday of month
+  let daySelected = '';
   let weekdayOfMonth = '';
 
-  if (selectedDate.getDay() - 1 < 0) {
+  // get weekday
+  if (firstDay.getDay() - 1 < 0) {
     weekdayOfMonth = weekdays[6];
   } else {
-    weekdayOfMonth = weekdays[selectedDate.getDay() - 1];
+    weekdayOfMonth = weekdays[firstDay.getDay() - 1];
   }
 
   let daysHTML = '';
 
+  // generate days
   weekdays.forEach((weekday) => {
-
     if (weekday === weekdayOfMonth) {
       for (let i = 1; i <= lastDay; i++) {
         let dayNumber = i;
@@ -185,6 +201,7 @@ function displayDays() {
 
         // console.log(`${i} === ${day} && ${month} === ${date.getMonth()}`);
 
+        // put special class for today's date
         if (i === day && month === date.getMonth()) {
           daysHTML += `
           <div class="day current-day" data-date="${daySelected}">${i}</div>
@@ -196,6 +213,7 @@ function displayDays() {
         }
       }
     } else {
+      // leave blank if first day doesn't have *this* weekday
       daysHTML += `<div class="empty"></div>`;
     }
   });
@@ -203,39 +221,41 @@ function displayDays() {
   document.querySelector('.days')
     .innerHTML = daysHTML;
 
-    document.querySelectorAll(('.day'))
-    .forEach((day) => {
-      day.addEventListener('click', () => {
-        const dayDate = day.dataset.date;
-        id = dayDate;
-        // show note panel
-        console.log(dayDate);
+  // for every element with 'day' class
+  // make day clickable  for note entries
+  document.querySelectorAll(('.day'))
+  .forEach((day) => {
+    day.addEventListener('click', () => {
+      const dayDate = day.dataset.date;
+      id = dayDate;
 
-        notePanel.classList.remove('hidden');
+      // show note panel
+      console.log(dayDate);
+      notePanel.classList.remove('hidden');
 
-        // display date
-        const dateSelected = document.getElementById('date-selected');
-        dateSelected.innerHTML = dayDate;
+      // display date
+      const dateSelected = document.getElementById('date-selected');
+      dateSelected.innerHTML = dayDate;
 
-        // display note
-        let matchingItem = '';
+      // display note
+      let matchingItem = '';
 
-        notes.forEach((note) => {
-          if (note.id === dayDate) {
-            matchingItem = note.input;
-          }
-        });
-
-        if (matchingItem) {
-          noteInput.value = matchingItem;
-        } else {
-          noteInput.value = '';
+      notes.forEach((note) => {
+        if (note.id === dayDate) {
+          matchingItem = note.input;
         }
       });
+
+      if (matchingItem) {
+        noteInput.value = matchingItem;
+      } else {
+        noteInput.value = '';
+      }
     });
+  });
 };
 
-// note panel ⏔⏔⏔
+// ⏔⏔⏔ note panel ⏔⏔⏔
 const notePanel = document.getElementById('note-panel');
 const closeNoteButton = document.getElementById('close-note-panel');
 
@@ -243,12 +263,14 @@ closeNoteButton.addEventListener('click', () => {
   notePanel.classList.add('hidden');
 });
 
-// save button
+// ⏔⏔⏔ save button ⏔⏔⏔
 const saveButton = document.getElementById('save-button');
 
 saveButton.addEventListener('click', () => {
   // see if the note entry for that day already exists
   notes.forEach((note) => {
+    // if current note id matches the id (got from clicking a day)
+    // store in an array
     if (note.id === id) {
       note.input = noteInput.value;
     } else {
@@ -263,5 +285,5 @@ saveButton.addEventListener('click', () => {
   localStorage.setItem('notes', JSON.stringify(notes));
 });
 
-// render when the page loads ⏔⏔⏔
+// ⏔⏔⏔ render when the page loads ⏔⏔⏔
 displayMonthYear(months[month]);
